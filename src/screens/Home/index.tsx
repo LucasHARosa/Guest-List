4c29829e9d2150e72a1498b3a160b4af66b81e88
lucasHARosa <l.ucas.henrique.a.r@gmail.com>
2023-05-10 15:52:28 -0300

    Finish

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,21 +2,31 @@ import { Alert, FlatList, ScrollView, Text, TextInput, TouchableOpacity, View } 
 
 import { styles } from "./styles";
 import { Participant } from "../../components/Participant";
+import { useState } from "react";
 
 export function Home() {
-  const participants = ['Rodrigo', 'Vini', 'Diego', 'Biro', 'Ana', 'Isa', 'Jack', 'Mayk', 'João'];
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [newParticipant, setNewParticipant] = useState("");
+
 
   function handleParticipantAdd() {
-    if (participants.includes("Rodrigo")) {
+    if (participants.includes(newParticipant)) {
       return Alert.alert("Participante existe", "Já existe um participante na lista com esse nome.");
     }
+    setParticipants(prevState => [...participants, newParticipant])
+    setNewParticipant("");
   }
 
   function handleParticipantRemove(name: string) {
+    const newArray = participants.filter((participant) => participant !== name)
+
+
     Alert.alert("Remover", `Remover o participante ${name}?`, [
       {
         text: 'Sim',
-        onPress: () => Alert.alert("Deletado!")
+        onPress: () => setParticipants(newArray),
+        
+
       },
       {
         text: 'Não',
@@ -27,11 +37,11 @@ export function Home() {
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
-        Nome do evento
+        Aniversário: Salão de festas do prédio
       </Text>
 
       <Text style={styles.eventDate}>
-        Sexta, 4 de Novembro de 2022.
+        Quarta-feira, 10 de Maio de 2023.
       </Text>
 
       <View style={styles.form}>
@@ -39,6 +49,8 @@ export function Home() {
           style={styles.input}
           placeholder="Nome do participante"
           placeholderTextColor="#6B6B6B"
+          onChangeText={setNewParticipant}
+          value={newParticipant}
         />
         
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
